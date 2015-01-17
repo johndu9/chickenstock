a4642e53538dbc1123a0686882a6a2b35ca91886
Tim Vincent <vincent3@purdue.edu>
2015-01-17 04:03:49 -0500

    Updated Bloomberg shell scripts

--- bloomberg/curlTest.sh
+++ bloomberg/curlTest.sh
@@ -1,5 +1,5 @@
 curl -X POST https://54.174.49.59/request/blp/refdata/HistoricalData --cacert ./bloomberg.crt --cert ./mhacks_spring_2015_058.crt --key ./mhacks_spring_2015_058.key --data @- <<EOF
-{ "securities": ["IBM US Equity", "AAPL US Equity"],
+{ "securities": ["IBM US Equity", "AAPL US Equity", "MSFT US Equity"],
   "fields": ["PX_LAST", "OPEN"],
   "startDate": "20120101",
   "endDate": "20120105",
