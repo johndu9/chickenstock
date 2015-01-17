daf977fe2165d2c24ab1bfbc9b28fc31adca82bb
mholmPurdue <mholm@purdue.edu>
2015-01-17 05:29:43 -0500

    frontend work

--- index.js
+++ index.js
@@ -41,3 +41,28 @@ function loadDoc() {
 			console.log(data);
 		});
 };
+
+function popObject(name, abbrv, price, rec, recText, buzz, buzzText){
+	$('#buzzBanner').css('background-color',buzz);
+	$('#buzzBanner').text(buzzText);
+	$('#stockName').text( abbrv );
+	$('#compName').text( name );
+	$('#price').text( "$" + price );
+	$('#recBanner').css('background-color', rec);
+	$('#recBanner').text(recText);
+	$('#content').slideToggle(400);
+}
+
+$( "#dismiss" ).click(function() {
+	var name, abbrv, price, rec, recText, buzz, buzzText;
+	name = "Google";
+	abbrv = "GOOG";
+	price = "3.50";
+	rec = "#00FF00"
+	recText = "DO IT"
+	buzz = "#FF0000";
+	buzzText = "THEY KILL PUPPIES"
+	$('#content').slideToggle(400, function() {
+		popObject(name, abbrv, price, rec, recText, buzz, buzzText);
+	})
+});
