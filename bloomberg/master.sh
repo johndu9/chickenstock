77d90e2c1a2efac8473a75cf828cd1ecac85df19
Tim Vincent <vincent3@purdue.edu>
2015-01-17 13:49:03 -0500

    Shrunk time to 1 month

--- bloomberg/master.sh
+++ bloomberg/master.sh
@@ -1,7 +1,7 @@
 curl -X POST https://54.174.49.59/request/blp/refdata/HistoricalData --cacert ./bloomberg.crt --cert ./mhacks_spring_2015_058.crt --key ./mhacks_spring_2015_058.key --data @- <<EOF
 { "securities": ["IBM US Equity", "AAPL US Equity", "MSFT US Equity", "GOOG US Equity", "T US Equity", "AMZN US Equity"],
   "fields": ["PX_LAST", "OPEN"],
-  "startDate": "20140601",
-  "endDate": "20150105",
+  "startDate": "20141201",
+  "endDate": "20150117",
   "periodicitySelection": "DAILY" }
 EOF
